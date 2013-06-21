461a540aacfbbb86f6ed43328fb458ffe36a956b
kizumba <guilhermevarela@hotmail.com>
2013-06-21 13:38:44 -0300

    Added mail behaviour Ajax is buggy

---
+++ app/mailers/company_mailer.rb
@@ -0,0 +1,17 @@
+class CompanyMailer < ActionMailer::Base
+  default from: "kiz0mba@yahoo.com.br"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.company_mailer.new_comment.subject
+  #
+  def new_comment(job, comment)
+    @job = job
+    @comment = comment
+    @company = job.company
+    @greeting = "Hi"
+
+    mail to: "kiz0mba@yahoo.com.br", subject: "New comment received" 
+  end
+end
