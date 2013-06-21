1ec5f6cedf7adeaa2e61312514cb2c87aa9afb98
kizumba <guilhermevarela@hotmail.com>
2013-06-20 23:24:21 -0300

    Created companies model

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,14 @@
+class CompaniesController < ApplicationController
+  def new
+    @company = Company.new
+  end
+  def create
+    @company = Company.new(params[:company])
+    if @company.save
+      redirect_to root_path, notice: "Company was saved!"
+    else
+      render action: "new"
+    end
+  end
+
+end
